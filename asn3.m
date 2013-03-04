8889ccad6a05a4ec9139b28e2b94196b15ef414c
Jaidonn <jfreela@uwo.ca>
2013-03-03 19:27:34 -0500

    Update asn3.m

--- asn3.m
+++ asn3.m
@@ -5,21 +5,21 @@
 !
 ! Description:
 !  	This file contain a program which takes an 9 digit number
-!		and runs it through Luhn's algorithm. It doubling even digits 
-!		by twoand if the doubled value is larger than 9, the composing  
-!		digit(10's and 1's column) are added togehter. This digit is then 
-!		added to a sum which is composed of the 9 digits. If there is a 
-!		remainder when diving the sum by 10, it is evluated as invalid and 
-!		a 'I' is outputed to the screen. If there is no remainder a 'V' is 
-!		ouputed to the screen.
+!	and runs it through Luhn's algorithm. It doubling even digits 
+!	by twoand if the doubled value is larger than 9, the composing  
+!	digit(10's and 1's column) are added togehter. This digit is then 
+!	added to a sum which is composed of the 9 digits. If there is a 
+!	remainder when diving the sum by 10, it is evluated as invalid and 
+!	a 'I' is outputed to the screen. If there is no remainder a 'V' is 
+!	ouputed to the screen.
 !
 ! Register Legend:
-! counter_r		imput counter				%l0
-! imput_r		user imput					%l1
-! storage_r		storage value				%l2
+! counter_r	imput counter			%l0
+! imput_r	user imput			%l1
+! storage_r	storage value			%l2
 ! firsthalf_r	stores half of the sum		%l3
 ! secondhalf_r	stores half of the sum		%l4
-! sum_r			total of imputed values		%l7
+! sum_r		total of imputed values		%l7
 !
 !**************************************************************	
 
@@ -35,25 +35,25 @@ define (sum_r,%l7)
 
 		.global main
 main:	save %sp, -96, %sp			!Create a location to save
-		clr counter_r				!Set counter to 0
-		clr sum_r					!Set sum to 0
+		clr counter_r			!Set counter to 0
+		clr sum_r			!Set sum to 0
 
 !***************************************************************
 !
 !***************************************************************		
 loop:	call getchar					!get imput value
 		nop
-		mov %o0,imput_r					!store value in imput_r
+		mov %o0,imput_r				!store value in imput_r
 		
 		mov imput_r, storage_r			!move imput value to another register
-		mov 2, %o0						!checking if imputed value is
-		call .rem						!divisable by 2. If so
-		bne double						!exits loop to go to doubleloop (if rem !=0)
-		be	sum							!exits loop to go to sum (if rem equal to 0)
+		mov 2, %o0				!checking if imputed value is
+		call .rem				!divisable by 2. If so
+		bne double				!exits loop to go to doubleloop (if rem !=0)
+		be	sum				!exits loop to go to sum (if rem equal to 0)
 		
-		inccc counter_r					!increment unsure_r
+		inccc counter_r				!increment unsure_r
 		subcc counter_r,NEWLINE,%g0		!compare unsure_r to 10. If unsure_r is
-		ble loop						!less than 10 retrun to loop
+		ble loop				!less than 10 retrun to loop
 		nop
 		
 		ba print
@@ -61,53 +61,53 @@ loop:	call getchar					!get imput value
 double:	
 		sll imput_r, 2, storage_r		!multiply the inputed value by two and storing it in a new register
 		subcc storage_r, 10, %g0		!set condition code (if imputed value >10)
-		bge	splitandadd					!exits double to go to splitandadd (greater than 10)
-		ble sum							!exits double to go to sum (less than 10)
-		
+		bge	splitandadd			!exits double to go to splitandadd (greater than 10)
+		ble sum					!exits double to go to sum (less than 10)
+	
 splitandadd:
 		mov storage_r, firsthalf_r		!move first character into firsthalf_r
-		call getchar					!get first character
+		call getchar				!get first character
 		nop
 		mov storage_r, secondhalf_r		!move second character into secondhalf_r
-		call get char					!get second character
+		call get char				!get second character
 		nop
 		
 		add firsthalf_r,secondhalf_r,storage_r	!Add the two parts together
-		ba sum									!Exit splitandadd to go to sum
+		ba sum					!Exit splitandadd to go to sum
 sum:
 		add sum_r, storage_r, sum_r		!add the stored value and sum. Storing as sum
-		ba loop							!Branch to loop
+		ba loop					!Branch to loop
 
 print:
 		if sum mod 10 = 0 print "V"
 		else print "I"
-		mov sum_r, %o0					!moves the sum to an output register 
-		mov 10, %o1						!checks is the sum is divisable by 10
+		mov sum_r, %o0				!moves the sum to an output register 
+		mov 10, %o1				!checks is the sum is divisable by 10
 		call .rem
-		bne invalid						!exits print to go to invalid(rem !=0)
-		be valid						!exits print to go to valid(rem is equal 0)
+		bne invalid				!exits print to go to invalid(rem !=0)
+		be valid				!exits print to go to valid(rem is equal 0)
 
 valid:		~how to print a v?
-		mov VALID, %o0					!move 'V' to an output register
-		call writeChar					!Print 'V' to the screen
+		mov VALID, %o0				!move 'V' to an output register
+		call writeChar				!Print 'V' to the screen
 		nop
 		
-		mov NEWLINE, %o1				!Move newline to output register
-		call writeChar					!Print a newline for formatting
+		mov NEWLINE, %o1			!Move newline to output register
+		call writeChar				!Print a newline for formatting
 		nop
 
-		ret								!Exit
+		ret					!Exit
 		restore
 		
 invalid:
-		mov INVALID, %o0						!Move 'I' to the output register
-		call writeChar					!Print 'I' to the screen
+		mov INVALID, %o0			!Move 'I' to the output register
+		call writeChar				!Print 'I' to the screen
 		nop
 		
-		mov NEWLINE, %o1				!Move newline to output register
-		call writeChar					!Print a newline for formatting
+		mov NEWLINE, %o1			!Move newline to output register
+		call writeChar				!Print a newline for formatting
 		nop
 
-		ret								!Exit
+		ret					!Exit
 		restore
 				
